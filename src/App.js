bd14df22ba8b0f4bc3b60161195938cdb318b26a
Miquel Castro <castro-miquel@hotmail.com>
2021-03-06 18:19:33 +0100

    Pruevas terminadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
+  function frases() {
+    const frases = [];
+    let color;
+    for(let i = 0; i < 100; i++){
+      color = `rgb(${i + 150}, ${i - 50}, ${i - 50})`
+      frases.push(<h1 style={{backgroundColor: color}} key={i}>Hola buenas {i + 1}</h1>);
+    }
+    return frases;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {frases()}
+    </React.Fragment>
   );
 }
 
