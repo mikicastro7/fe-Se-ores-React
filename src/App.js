d7f68ab448fcbcda3e404dcfb76e2ab3d9c8dd56
Miquel Castro <castro-miquel@hotmail.com>
2021-03-06 18:27:35 +0100

    Comprobar los cambios

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
     let color;
     for(let i = 0; i < 100; i++){
       color = `rgb(${i + 150}, ${i - 50}, ${i - 50})`
-      frases.push(<h1 style={{backgroundColor: color}} key={i}>Hola buenas {i + 1}</h1>);
+      frases.push(<p style={{backgroundColor: color}} key={i}>Hola buenas {i + 1}</p>);
     }
     return frases;
   }
 
   return (
     <React.Fragment>
+      <h1>Hola buenas Titulo</h1>
       {frases()}
     </React.Fragment>
   );
